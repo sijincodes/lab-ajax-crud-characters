6f982a69ff55d4a6a9bcb4922c87d5a94cd073b4
sijincodes <sijinmathews03@gmail.com>
2022-11-17 22:26:27 +0100

    iteration 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,37 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList() {
+    return await axios
+      .get(this.BASE_URL + "/characters")
+      .then((responseFromApi) => responseFromApi.data);
   }
 
-  getOneRegister () {
-
+  async getOneRegister(id) {
+    return await axios
+      .get(this.BASE_URL + "/characters/" + id)
+      .then((responseFromApi) => responseFromApi.data);
   }
 
-  createOneRegister () {
-
+  async createOneRegister(characterObj) {
+    return await axios
+      .post(this.BASE_URL + "/characters", characterObj)
+      .then((characterObj) => characterObj.data);
   }
 
-  updateOneRegister () {
-
+ async updateOneRegister(id,characterObj) {
+    return await axios
+      .put(this.BASE_URL + "/characters/" + id,characterObj)
+      .then((characterObj) => characterObj.data);
   }
 
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    return await axios
+      .delete(this.BASE_URL + "/characters/" + id)
+     // res.send(`Character has been successfully deleted " ${req.data.name}`))
+       .then(() => "Character has been successfully deleted ");
+    // res.redirect(`/movie-characters/list`); // <== leave this line commented for now
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,74 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+       console.log(charactersAPI.getFullList());
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      // console.log(
+      //   charactersAPI.getOneRegister(
+    //  Number(document.querySelector('input[name="character-id"]').value)
+      //   )
+      // );
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {
+        console.log(
+        charactersAPI.deleteOneRegister(
+     Number(document.querySelector('input[name="character-id-delete"]').value)
+        ))
+      
+    });
 
-  });
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {
+      const id = document.querySelector(
+        '#edit-character-form input[name="chr-id"]'
+      ).value;
+      const updatedName = document.querySelector(
+        '#edit-character-form input[name="name"]'
+      ).value;
+      const updatedOccupation = document.querySelector(
+        '#edit-character-form input[name="occupation"]'
+      ).value;
+      const isCartoonUpdate = document.querySelector(
+        '#edit-character-form input[name="cartoon"]'
+      ).checked;
+      const updatedWeapon = document.querySelector(
+        '#edit-character-form input[name="weapon"]'
+      ).value;
+      charactersAPI.updateOneRegister(Number(id),{updatedName, updatedOccupation,updatedWeapon,isCartoonUpdate})
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit",  function (event) {
+      const name = document.querySelector(
+        '#new-character-form input[name="name"]'
+      ).value;
+      const occupation = document.querySelector(
+        '#new-character-form input[name="occupation"]'
+      ).value;
+      const isCartoon = document.querySelector(
+        '#new-character-form input[name="cartoon"]'
+      ).checked;
+      const weapon = document.querySelector(
+        '#new-character-form input[name="weapon"]'
+      ).value;
+      // console.log(isCartoon +"if cartton y/n");
+      // console.log(name, ' name')
+      // console.log((occupation, ' occup'));
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+     // console.log(
+       charactersAPI.createOneRegister({name, occupation,weapon,isCartoon})
+      //);
+    });
 });
